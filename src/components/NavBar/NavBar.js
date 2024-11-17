2cc4f1c3480fa5e6881105dca85cf9587068f675
NatanelSmadja <motivatoffice@gmail.com>
2024-11-17 02:41:53 +0200

    Update Navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -10,6 +10,7 @@ import {
   where,
   onSnapshot,
   updateDoc,
+  arrayUnion,
   addDoc,
 } from "firebase/firestore";
 import { signOut } from "firebase/auth";
@@ -24,20 +25,23 @@ import Search from "../Search/Search";
 
 const NavBar = () => {
   const navigate = useNavigate();
-  const { currentUser } = useAuth();
   const [isVerified, setIsVerified] = useState(false);
   const [friendRequests, setFriendRequests] = useState([]);
-  const [notifications, setNotifications] = useState([]);
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [dropdownOpen2, setDropdownOpen2] = useState(false);
+  const [notfications, setNotifications] = useState([]);
+  const { currentUser } = useAuth();
+  const [dropdownOpen, setDropdownOpen] = useState(false); // Managing the dropdown state
+  const [dropdownOpen2, setDropdownOpen2] = useState(false); // Managing the dropdown state
   const [menuOpen, setMenuOpen] = useState(false);
   const [unreadCount, setUnreadCount] = useState(0);
+  const [users, setUsers] = useState([]);
   const [unreadMessages, setUnreadMessages] = useState(0);
 
+  //function that toggle the hamburger menu
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
+  //logout function for quit from user.
   const handleLogout = async () => {
     signOut(auth)
       .then(() => {
@@ -47,6 +51,118 @@ const NavBar = () => {
       .catch((error) => console.log("Error Sign Out"));
   };
 
+  // Accept friend request
+  const handleAccept = async (request) => {
+    try {
+      const requestDocRef = doc(db, "friendRequests", request.id);
+
+      // Update the status in Firebase to "accepted"
+      await updateDoc(requestDocRef, { status: "accepted" });
+
+      // Update the friends list for both users
+      await updateDoc(doc(db, "Users", currentUser.uid), {
+        friends: arrayUnion(request.senderId),
+      });
+      await updateDoc(doc(db, "Users", request.senderId), {
+        friends: arrayUnion(currentUser.uid),
+      });
+
+      // Send a notification to the user who sent the friend request
+      const receiverUserDocRef = doc(db, "Users", request.senderId);
+      try {
+        const docSnap = await getDoc(receiverUserDocRef);
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          const receiverName = `${data.firstName} ${data.lastName}`;
+          console.log(data); //natan    currentUser=elianor
+          await newFriendNotification(data.uid, currentUser);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    } catch (error) {
+      console.error("Error accepting friend request:", error);
+    }
+  };
+
+  // notification for new friend and adding it to firebase
+  const newFriendNotification = async (newFriendId, acceptedUser) => {
+    const notification = {
+      postUser: newFriendId,
+      newFriendId: acceptedUser.uid,
+      newFriendName:
+        acceptedUser.type === "student"
+          ? `${acceptedUser.userName}` //to student name
+          : `${acceptedUser.companyName}`, // to company name
+      type: "new friend",
+    };
+    const notificationsRef = addDoc(
+      collection(db, "Notifications"),
+      notification
+    )
+      .then((res) => {
+        console.log("Document has been added succesfully");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // Reject friend request
+  const handleReject = async (request) => {
+    try {
+      const requestDocRef = doc(db, "friendRequests", request.id);
+      await updateDoc(requestDocRef, { status: "rejected" });
+    } catch (error) {
+      console.error("Error rejecting friend request:", error);
+    }
+  };
+
+  // Toggle dropdown
+  const toggleDropdown = () => {
+    console.log("is toggle");
+    
+    setDropdownOpen(!dropdownOpen);
+    if (dropdownOpen2) setDropdownOpen2(!dropdownOpen2);
+  };
+  // Toggle dropdown
+  const toggleDropdown2 = () => {
+    setDropdownOpen2(!dropdownOpen2);
+    if (dropdownOpen) setDropdownOpen(!dropdownOpen);
+  };
+  const clearNotifications = async () => {
+    try {
+      const q = query(
+        collection(db, "Notifications"),
+        where("postUser", "==", currentUser.uid)
+      );
+
+      setNotifications([]); //Clear the notification in the local state
+      console.log("Notifications cleared!");
+    } catch (error) {
+      console.error("Error clearing notifications:", error);
+    }
+  };
+
+  const handleNotificationClick = () => {
+    toggleDropdown2(); // close or open the dropdown
+
+    if (unreadCount > 0) {
+      //update from unread to reade
+      notfications.forEach(async (notification) => {
+        if (!notification.read) {
+          const notificationRef = doc(db, "Notifications", notification.id);
+          await updateDoc(notificationRef, { read: true });
+        }
+      });
+
+      // reset the count for 0
+      setUnreadCount(0);
+    }
+  };
+
+  //useEffect
+  //**useEffect 1 for check verification if the user have an access and got verified.
   useEffect(() => {
     const checkVerification = async () => {
       if (currentUser) {
@@ -57,28 +173,59 @@ const NavBar = () => {
         }
       }
     };
+
     checkVerification();
   }, [currentUser]);
 
+  //**useEffect 2 for notifications
   useEffect(() => {
     if (currentUser) {
-      const notificationsRef = collection(db, "Notifications");
-      const q = query(notificationsRef, where("userId", "==", currentUser.uid));
+      const q = query(
+        collection(db, "friendRequests"),
+        where("receiverId", "==", currentUser.uid),
+        where("status", "==", "pending")
+      );
 
       const unsubscribe = onSnapshot(q, (snapshot) => {
-        const newNotifications = snapshot.docs.map((doc) => ({
+        const requests = snapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
         }));
-
-        setNotifications(newNotifications);
-        setUnreadCount(newNotifications.filter((n) => !n.read).length);
+        console.log(requests);
+        console.log(requests);
+        setFriendRequests(requests);
       });
 
       return () => unsubscribe();
     }
   }, [currentUser]);
 
+  useEffect(() => {
+    if (currentUser) {
+      const q = query(
+        collection(db, "Notifications"),
+        where("postUser", "==", currentUser.uid)
+      );
+
+      const unsubscribe = onSnapshot(q, (docSnapshot) => {
+        const allNotifications = docSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setNotifications(allNotifications);
+
+        // עדכון מספר ההתראות שלא נקראו (לדוגמה: אם כל התראות חדשות)
+        const unread = allNotifications.filter(
+          (notification) => !notification.read
+        ).length;
+        setUnreadCount(unread);
+      });
+
+      return () => unsubscribe();
+    }
+  }, [currentUser]);
+
+  //useEffect 3 for conversations
   useEffect(() => {
     if (!currentUser) return;
     const fetchUnreadMessage = () => {
@@ -118,6 +265,59 @@ const NavBar = () => {
 
     fetchUnreadMessage();
   }, [currentUser]);
+
+  //*useEffect 4
+  useEffect(() => {
+    if (currentUser) {
+      const singleQuery = query(collection(db, "Notifications"));
+      const unsubscribe = onSnapshot(singleQuery, async (response) => {
+        const notificationsData = [];
+        for (const docN of response.docs) {
+          const notificationData = docN.data();
+          let userDoc;
+          if (
+            notificationData.type === "comment" &&
+            notificationData.postUser === currentUser.uid
+          ) {
+            userDoc = await getDoc(
+              doc(db, "Users", notificationData.commentId)
+            );
+          } else if (
+            notificationData.type === "like" &&
+            notificationData.postUser === currentUser.uid
+          ) {
+            userDoc = await getDoc(doc(db, "Users", notificationData.likeId));
+          } else if (notificationData.type === "new friend") {
+            userDoc = await getDoc(
+              doc(db, "Users", notificationData.newFriendId)
+            );
+          } else {
+            if (notificationData && notificationData.user) {
+              userDoc = await getDoc(doc(db, "Users", notificationData.user));
+            }
+          }
+          const userName =
+            userDoc && userDoc.exists()
+              ? `${userDoc.data().userName}`
+              : "Unknown User";
+          const userProfilePicture =
+            userDoc && userDoc.exists()
+              ? userDoc.data().profilePicture
+              : "defaultProfilePictureURL"; // Default picture if not available
+          notificationsData.push({
+            id: docN.id,
+            ...notificationData,
+            userName,
+            userProfilePicture,
+          });
+        }
+        setNotifications(notificationsData); // Set notifications with user info
+      });
+      return () => unsubscribe(); // Clean up listener
+    }
+  }, [currentUser]);
+
+  //End of useEffect
 
   return (
     <header className="bg-white shadow-sm  w-full top-0 z-50">
@@ -168,7 +368,7 @@ const NavBar = () => {
                 <li className="relative py-2 px-4 cursor-pointer hover:border-b-2 hover:border-[#15CDCA] transition">
                   <Link to="/chats">
                     <div className="relative">
-                      <BsChatDots size={24} /> 
+                      <BsChatDots size={24} />
                       {unreadMessages > 0 && (
                         <span className="absolute top-0 right-0 transform translate-x-2 -translate-y-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
                           {unreadMessages}
@@ -177,12 +377,12 @@ const NavBar = () => {
                     </div>
                   </Link>
                 </li>
-
+                        {/* Notification icon (the bell) */}
                 <li
                   onClick={() => setDropdownOpen2(!dropdownOpen2)}
                   className="relative py-2 px-4 cursor-pointer hover:border-b-2 hover:border-[#15CDCA] transition"
                 >
-                  <BsBell size={24} />
+                  <BsBell size={24} onClick={handleNotificationClick} />
                   {unreadCount > 0 && (
                     <span className="absolute top-0 right-0 bg-red-600 text-white rounded-full text-xs px-1.5">
                       {unreadCount}
@@ -190,14 +390,98 @@ const NavBar = () => {
                   )}
                   {dropdownOpen2 && (
                     <div className="absolute left-1/2 transform -translate-x-1/2 mt-2 top-12 w-64 bg-white border rounded-lg shadow-lg z-10">
-                      {notifications.length > 0 ? (
-                        notifications.map((notification, index) => (
+                      {notfications.length > 0 ? (
+                        notfications.map((notification, index) => (
                           <div
-                            key={index}
-                            className="p-2 text-gray-700 hover:bg-gray-100"
-                          >
-                            {notification.message}
-                          </div>
+                          key={index}
+                          className="px-4 py-2 text-gray-700 hover:bg-gray-100"
+                        >
+                          {/*notification type*/}
+                          {/* comment */}
+                          {notification.type === "comment" && (
+                            <>
+                              <Link
+                                className="flex mb-2 items-center gap-2"
+                                to={`/post/${notification.postId}`}
+                              >
+                                <img
+                                  className="w-10 h-10 rounded-full"
+                                  src={
+                                    notification.userProfilePicture ||
+                                    "defaultProfilePictureURL"
+                                  } // הצגת תמונת המשתמש הנכונה מהתגובה
+                                  alt={`${users.userName}`}
+                                />
+                                {`${notification.commentName} הוסיף תגובה לפוסט שלך`}
+                              </Link>
+                              <hr />
+                            </>
+                          )}
+                          {/* Like */}
+                          {notification.type === "like" && (
+                            <>
+                              <Link
+                                className="flex mb-2 items-center gap-2"
+                                to={`/post/${notification.postId}`}
+                              >
+                                <img
+                                  className="w-10 h-10 rounded-full"
+                                  src={
+                                    notification.userProfilePicture ||
+                                    "defaultProfilePictureURL"
+                                  } // הצגת תמונת המשתמש הנכונה מהתגובה
+                                  alt={`${users.userName}`}
+                                />
+                                {`${notification.likeName} אהב את הפוסט שלך`}
+                              </Link>
+                              <hr />
+                            </>
+                          )}
+                          {/* friend */}
+                          {notification.type === "new friend" && (
+                            <>
+                              <Link
+                                className="flex mb-2 items-center gap-2"
+                                to={`/profile/${notification.newFriendId}`}
+                              >
+                                <img
+                                  className="w-10 h-10 rounded-full"
+                                  src={
+                                    notification.userProfilePicture ||
+                                    "defaultProfilePictureURL"
+                                  } // הצגת תמונת המשתמש הנכונה מהתגובה
+                                  alt={`${users.userName}`}
+                                />
+                                {notification.newFriendName} אישר/ה את בקשת
+                                החברות שלך
+                              </Link>
+                              <hr />
+                            </>
+                          )}
+                          {/* mission */}
+                          {notification.type === "new Mission" && (
+                            <>
+                              <Link
+                                className="flex mb-2 items-center gap-2"
+                                to={`/mission/${notification.missionId}`}
+                              >
+                                <img
+                                  className="w-10 h-10 rounded-full"
+                                  src={
+                                    notification.userProfilePicture ||
+                                    "defaultProfilePictureURL"
+                                  } // הצגת תמונת המשתמש הנכונה מהתגובה
+                                  alt={`${users.userName}`}
+                                />
+                                {notification.postUserName} הוסף משימה:
+                                {notification.missionTitle}
+                              </Link>
+                              <hr />
+                            </>
+                            
+                          )}
+                          
+                        </div>
                         ))
                       ) : (
                         <div className="p-2 text-gray-700 text-center">
@@ -206,17 +490,58 @@ const NavBar = () => {
                       )}
                     </div>
                   )}
-                </li>
+                </li> {/*End of the notification icon  */}
+
+                {/* friend request icon */}
                 <li
                   onClick={() => setDropdownOpen(!dropdownOpen)}
                   className="relative py-2 px-4 cursor-pointer hover:border-b-2 hover:border-[#15CDCA] transition"
                 >
-                  <LiaUserFriendsSolid size={24} />
+                  <LiaUserFriendsSolid size={24} onClick={toggleDropdown} />
                   {friendRequests.length > 0 && (
                     <span className="absolute top-0 right-0 bg-red-600 text-white rounded-full text-xs px-1.5">
                       {friendRequests.length}
                     </span>
                   )}
+                   {dropdownOpen && (
+                      <div className="absolute right-2 top-[40%] mt-2 w-80 bg-white border rounded-lg shadow-lg z-10">
+                        {friendRequests.length > 0 ? (
+                          friendRequests.map((request) => (
+                            <div
+                              key={request.id}
+                              className="px-4 py-2 text-gray-700 hover:bg-gray-100"
+                            >
+                              <p className="flex items-center gap-2">
+                                <img
+                                  className="w-8 h-8 rounded-full"
+                                  src={request.senderPicture}
+                                  alt={request.senderFirstName}
+                                />
+                                {request.senderuserName} שלח לך בקשת חברות
+                              </p>
+                              <div className="flex justify-end gap-2 mt-2">
+                                <button
+                                  className="bg-green-500 text-white px-3 py-1 rounded hover:bg-green-600"
+                                  onClick={() => handleAccept(request)}
+                                >
+                                  אישור
+                                </button>
+                                <button
+                                  className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
+                                  onClick={() => handleReject(request)}
+                                >
+                                  דחייה
+                                </button>
+                              </div>
+                            </div>
+                          ))
+                        ) : (
+                          <div className="px-4 py-2 text-gray-700">
+                            אין בקשות חדשות
+                          </div>
+                        )}
+                      </div>
+                    )}
                 </li>
               </>
             ) : (
