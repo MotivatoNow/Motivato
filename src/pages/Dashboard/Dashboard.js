2263a1f21f6dd1d9c61b485dff50d532e35312f6
elianorz <motivatoffice@gmail.com>
2025-01-06 15:45:27 +0200

    Dashboard solve

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { db } from "../../config/firebase";
-import { collection, getDocs, updateDoc, doc, deleteDoc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  updateDoc,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 import AddCategory from "../../features/AddCategory/AddCategory";
 import AddUniversity from "../../features/AddUniversity/AddUniversity";
 import { useAuth } from "../../context/AuthContext";
@@ -16,21 +22,28 @@ const Dashboard = () => {
   const [loading, setLoading] = useState(true);
   const [isOpenCategory, setIsOpenCategory] = useState(false);
   const [onCloseCategory, setOnCloseCategory] = useState(false);
-  const [category,setCategory]=useState(null)
+  const [category, setCategory] = useState(null);
   const [isOpenUniversity, setIsOpenUniversity] = useState(false);
   const [onCloseUniversity, setOnCloseUniversity] = useState(false);
-  const [university,setUniversity]=useState(null)
-  
-  
+  const [university, setUniversity] = useState(null);
+
   useEffect(() => {
     const fetchData = async () => {
       const usersSnapshot = await getDocs(collection(db, "Users"));
       const categoriesSnapshot = await getDocs(collection(db, "Categories"));
-      const universitiesSnapshot = await getDocs(collection(db, "Universities"));
+      const universitiesSnapshot = await getDocs(
+        collection(db, "Universities")
+      );
 
-      setUsers(usersSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
-      setCategories(categoriesSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
-      setUniversities(universitiesSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+      setUsers(
+        usersSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+      );
+      setCategories(
+        categoriesSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+      );
+      setUniversities(
+        universitiesSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+      );
       setLoading(false);
     };
 
@@ -57,19 +70,19 @@ const Dashboard = () => {
     setUsers(users.filter((user) => user.id !== id));
   };
 
-  const handleEditCategory=async(category)=>{
-    setIsOpenCategory(true)
-    setCategory(category)
-  }
+  const handleEditCategory = async (category) => {
+    setIsOpenCategory(true);
+    setCategory(category);
+  };
   const handleDeleteCategory = async (id) => {
     const categoryDoc = doc(db, "Categories", id);
     await deleteDoc(categoryDoc);
     setCategories(categories.filter((category) => category.id !== id));
   };
-  const handleEditUniversity=async(university)=>{
-    setIsOpenUniversity(true)
-    setUniversity(university)
-  }
+  const handleEditUniversity = async (university) => {
+    setIsOpenUniversity(true);
+    setUniversity(university);
+  };
   const handleDeleteUniversity = async (id) => {
     const universityDoc = doc(db, "Universities", id);
     await deleteDoc(universityDoc);
@@ -77,7 +90,9 @@ const Dashboard = () => {
   };
 
   if (loading) {
-    return <div className="flex justify-center items-center h-screen">טוען...</div>;
+    return (
+      <div className="flex justify-center items-center h-screen">טוען...</div>
+    );
   }
 
   if (currentUser.userType !== "Admin") {
@@ -104,7 +119,9 @@ const Dashboard = () => {
     <div className="flex h-screen">
       {/* Tabs Navigation */}
       <div className="w-1/4 bg-gray-100 p-4 border-l border-gray-200">
-        <h2 className="text-lg font-medium mb-4 text-center text-gray-700">אפשרויות</h2>
+        <h2 className="text-lg font-medium mb-4 text-center text-gray-700">
+          אפשרויות
+        </h2>
         {tabs.map((tab) => (
           <button
             key={tab.id}
@@ -121,7 +138,9 @@ const Dashboard = () => {
       </div>
       {/* Content Area */}
       <div className="w-3/4 p-6 overflow-auto">
-        <h1 className="text-2xl font-semibold text-gray-800 mb-6">Admin Dashboard</h1>
+        <h1 className="text-2xl font-semibold text-gray-800 mb-6">
+          Admin Dashboard
+        </h1>
 
         {activeTab === "newRegistrations" && (
           <div>
@@ -218,52 +237,71 @@ const Dashboard = () => {
             <AddCategory setCategories={setCategories} />
             <h3 className="text-md font-medium mt-6 mb-2">קטגוריות קיימות:</h3>
             <ul className="list-disc pl-6">
-            {categories.map((category) => (
-  <li key={category.id} className="flex justify-between items-center mt-3">
-    <span>{category.nameCategory}</span>
-   <div className="flex gap-1">
-   <button
-      onClick={() => handleEditCategory(category)}
-      className="bg-blue-500 text-white px-3 py-1 rounded-md hover:bg-blue-600"
-    >
-      Edit
-    </button>
-    <button  className="bg-red-500 text-white px-3 py-1 rounded-md hover:bg-red-600" onClick={()=> handleDeleteCategory(category.id)}>מחיקת קטגוריה</button>
-   </div>
-  </li>
-))}
+              {categories.length > 0 ? (
+                <ul className="list-disc pl-6">
+                  {categories.map((category) => (
+                    <li
+                      key={category.id}
+                      className="flex justify-between items-center mt-3"
+                    >
+                      <span>{category.nameCategory}</span>
+                      <div className="flex gap-1">
+                        <button
+                          onClick={() => handleEditCategory(category)}
+                          className="bg-blue-500 text-white px-3 py-1 rounded-md hover:bg-blue-600"
+                        >
+                          Edit
+                        </button>
+                        <button
+                          className="bg-red-500 text-white px-3 py-1 rounded-md hover:bg-red-600"
+                          onClick={() => handleDeleteCategory(category.id)}
+                        >
+                          מחיקת קטגוריה
+                        </button>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p className="text-gray-500 mt-4">אין קטגוריות זמינות.</p>
+              )}
             </ul>
           </div>
         )}
 
-{activeTab === "addUniversity" && (
-  <div>
-    <h2 className="text-lg font-medium mb-4">הוספת אוניברסיטאות</h2>
-    <AddUniversity setUniversities={setUniversities} />
-    <h3 className="text-md font-medium mt-6 mb-2">אוניברסיטאות קיימות:</h3>
-    <ul className="list-disc pl-6">
-      {universities.map((university) => (
-        <li key={university.id} className="flex justify-between items-center mt-3">
-          <span>{university.nameUniversity}</span>
-          <div className="flex gap-1">
-            <button
-              onClick={() => handleEditUniversity(university)}
-              className="bg-blue-500 text-white px-3 py-1 rounded-md hover:bg-blue-600"
-            >
-              Edit
-            </button>
-            <button
-              onClick={() => handleDeleteUniversity(university.id)}
-              className="bg-red-500 text-white px-3 py-1 rounded-md hover:bg-red-600"
-            >
-              מחיקת מוסד לימודי
-            </button>
+        {activeTab === "addUniversity" && (
+          <div>
+            <h2 className="text-lg font-medium mb-4">הוספת אוניברסיטאות</h2>
+            <AddUniversity setUniversities={setUniversities} />
+            <h3 className="text-md font-medium mt-6 mb-2">
+              אוניברסיטאות קיימות:
+            </h3>
+            <ul className="list-disc pl-6">
+              {universities.map((university) => (
+                <li
+                  key={university.id}
+                  className="flex justify-between items-center mt-3"
+                >
+                  <span>{university.nameUniversity}</span>
+                  <div className="flex gap-1">
+                    <button
+                      onClick={() => handleEditUniversity(university)}
+                      className="bg-blue-500 text-white px-3 py-1 rounded-md hover:bg-blue-600"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => handleDeleteUniversity(university.id)}
+                      className="bg-red-500 text-white px-3 py-1 rounded-md hover:bg-red-600"
+                    >
+                      מחיקת מוסד לימודי
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
-        </li>
-      ))}
-    </ul>
-  </div>
-)}
+        )}
 
         {activeTab === "statistics" && (
           <div>
@@ -280,17 +318,17 @@ const Dashboard = () => {
         )}
       </div>
       <ModalCategories
-  isOpen={isOpenCategory}
-  onClose={() => setIsOpenCategory(false)}
-  setCategories={setCategories}
-  categories={category}
-/>
-<ModalUniversity
-  isOpen={isOpenUniversity}
-  onClose={() => setIsOpenUniversity(false)}
-  setUniversity={setUniversities}
-  universities={university}
-/>
+        isOpen={isOpenCategory}
+        onClose={() => setIsOpenCategory(false)}
+        setCategories={setCategories}
+        categories={category}
+      />
+      <ModalUniversity
+        isOpen={isOpenUniversity}
+        onClose={() => setIsOpenUniversity(false)}
+        setUniversity={setUniversities}
+        universities={university}
+      />
     </div>
   );
 };
