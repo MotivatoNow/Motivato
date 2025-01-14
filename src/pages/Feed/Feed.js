4ad2950010a74290e927c5267a9a66a0f45c537b
elianorz <motivatoffice@gmail.com>
2025-01-14 10:35:37 +0200

    feed design

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,44 +1,76 @@
-import React, { useState, useMemo, useEffect } from "react";
+import React, { useState, useMemo } from "react";
 import MyPost from "../../components/MyPost/MyPost";
 import "./Feed.css";
 import PostCard from "../../components/PostCard/PostCard";
 import { useAuth } from "../../context/AuthContext";
 import RightSide from "../../components/Feed/RightSide/RightSide";
-import LeftSide from "../../components/Feed/LeftSide/LeftSide"
+import LeftSide from "../../components/Feed/LeftSide/LeftSide";
 import { loadPosts } from "../../hooks/useLoadPosts";
 
 const Feed = () => {
   const { currentUser } = useAuth();
   const [allPosts, setAllPosts] = useState([]);
+  const [rightScrolling, setRightScrolling] = useState(false);
+  const [leftScrolling, setLeftScrolling] = useState(false);
+  
   useMemo(() => {
     loadPosts(setAllPosts);
   }, []);
+  
+  const handleMouseEnterRight = () => setRightScrolling(true);
+  const handleMouseLeaveRight = () => setRightScrolling(false);
+
+  const handleMouseEnterLeft = () => setLeftScrolling(true);
+  const handleMouseLeaveLeft = () => setLeftScrolling(false);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-4 gap-4 bg-[#F6F9FC] w-full mx-auto ">
-  {/* עמודת פרופיל - צד שמאל */}
-  <div className="hidden lg:block  lg:col-span-1">
-          <RightSide />
-  </div>
+      {/* עמודת פרופיל - צד שמאל */}
+      <div
+        className={`hidden lg:block lg:col-span-1 transition-all duration-300 ${
+          rightScrolling ? "overflow-y-auto max-h-screen" : "overflow-hidden"
+        }`}
+        onMouseEnter={handleMouseEnterRight}
+        onMouseLeave={handleMouseLeaveRight}
+        style={{
+          position: 'sticky', 
+          top: '0',  
+          height: '100vh',
+        }}
+        >
+        <RightSide />
+      </div>
 
-  {/* עמודת הפיד המרכזית */}
-  <div className="col-span-1 lg:col-span-2">
-    <div>
-      <MyPost />
-      <div>
-        {allPosts.map((post) => (
-          <div key={post.id}>
-            <PostCard posts={post} user={currentUser} />
+      {/* עמודת הפיד המרכזית */}
+      <div className="col-span-1 lg:col-span-2">
+        <div>
+          <MyPost />
+          <div>
+            {allPosts.map((post) => (
+              <div key={post.id}>
+                <PostCard posts={post} user={currentUser} />
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
+      </div>
+
+      {/* עמודת פרופיל - צד ימין */}
+      <div
+        className={`hidden lg:block lg:col-span-1 transition-all duration-300 ${
+          leftScrolling ? "overflow-y-auto max-h-screen" : "overflow-hidden"
+        }`}
+        onMouseEnter={handleMouseEnterLeft}
+        onMouseLeave={handleMouseLeaveLeft}
+        style={{
+          position: 'sticky', 
+          top: '0', 
+          height: '100vh',  
+        }}
+        >
+        <LeftSide />
       </div>
     </div>
-  </div>
-
-  <div className="hidden lg:block  lg:col-span-1">
-          <LeftSide/>
-  </div>
-</div>
-
   );
 };
 
