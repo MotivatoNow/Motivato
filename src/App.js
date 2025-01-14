f3260bfead16cb32bf983773f36ad9e9284d0031
elianorz <motivatoffice@gmail.com>
2025-01-14 09:48:21 +0200

    login solve

--- src/App.js
+++ src/App.js
@@ -1,53 +1,72 @@
-import './assets/styles/App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar';
+import "./assets/styles/App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
 
-import HomePage from './pages/Home/Home';
-import LoginPage from './pages/Login/Login'; // רכיב התחברות
-import RegisterPage from './pages/Register/Register';
-import Profile from './pages/Profile/Profile';
-import {AuthProvider} from './context/AuthContext';
-import React from 'react';
+import HomePage from "./pages/Home/Home";
+import LoginPage from "./pages/Login/Login"; // רכיב התחברות
+import RegisterPage from "./pages/Register/Register";
+import Profile from "./pages/Profile/Profile";
+import { AuthProvider, useAuth } from "./context/AuthContext";
+import React from "react";
 import Dashboard from "./pages/Dashboard/Dashboard";
-import ForgetPassword from './pages/ForgetPassword/ForgetPassword';
-import Feed from './pages/Feed/Feed';
+import ForgetPassword from "./pages/ForgetPassword/ForgetPassword";
+import Feed from "./pages/Feed/Feed";
 import ProtectedRoute from "./features/ProtectedRoute/ProtectedRoute";
-import ChatOverview  from "./pages/ChatOverview/ChatOverview"
+import ChatOverview from "./pages/ChatOverview/ChatOverview";
 import Post from "./pages/Post/Post";
-import  MissionFeed from "./pages/MissionFeed/MissionFeed";
-import Mission from "./pages/Mission/Mission"
-import FollowersView from './pages/FollowersView/FollowersView';
+import MissionFeed from "./pages/MissionFeed/MissionFeed";
+import Mission from "./pages/Mission/Mission";
+import FollowersView from "./pages/FollowersView/FollowersView";
 
 function App() {
-
-    return (
-        <>
-            <AuthProvider>
-                <Router>
-                    <NavBar />
-                    <Routes>
-                        <Route path="/" element={<HomePage/>}/>
-                        <Route path="/login" element={<LoginPage/>}/>
-                        <Route path="/register" element={<RegisterPage/>}/>
-                        <Route path="/reset" element={<ForgetPassword/>}/>
-                        {/*******/}
-                        <Route path="/admin-dashboard" element={<Dashboard/>}/>
-                        {/*******/}
-                        <Route path="/feed" element={<ProtectedRoute><Feed/></ProtectedRoute>}/>
-                        <Route path="/chats" element={<ChatOverview />}/>
-                        <Route path="/missions" element={<ProtectedRoute><MissionFeed/></ProtectedRoute>}/>
-                        {/*******/}
-                        <Route path="/profile/:id" element={<ProtectedRoute><Profile/></ProtectedRoute>} />
-                        <Route path="/post/:id" element={<Post/>}/>
-                        <Route path="/mission/:id" element={<Mission/>}/>
-                        <Route path="/followers/:id" element={<FollowersView/>}/>
-                        
-                    </Routes>
-                </Router>
-            </AuthProvider>
-
-        </>
-    );
+  
+  return (
+    <>
+      <AuthProvider>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/reset" element={<ForgetPassword />} />
+            {/*******/}
+            <Route path="/admin-dashboard" element={<Dashboard />} />
+            {/*******/}
+            <Route
+              path="/feed"
+              element={
+                <ProtectedRoute>
+                  <Feed />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/chats" element={<ChatOverview />} />
+            <Route
+              path="/missions"
+              element={
+                <ProtectedRoute>
+                  <MissionFeed />
+                </ProtectedRoute>
+              }
+            />
+            {/*******/}
+            <Route
+              path="/profile/:id"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/post/:id" element={<Post />} />
+            <Route path="/mission/:id" element={<Mission />} />
+            <Route path="/followers/:id" element={<FollowersView />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </>
+  );
 }
 
 export default App;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,50 +1,81 @@
-// src/pages/Login/Login.js
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { auth } from '../../config/firebase';
-import { useAuth } from '../../context/AuthContext';
-import './Login.css';
-import { handleLogin } from '../../hooks/useLogin';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import "./Login.css";
+import { auth, db } from "../../config/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
+  const { currentUser } = useAuth();
 
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+
+      // בדוק את מצב האימות של המשתמש
+      const userDoc = await getDoc(doc(db, "Users", user.uid));
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+        if (!userData.isVerified) {
+          setError("יש להמתין לאישור המערכת לפני התחברות.");
+          await auth.signOut(); // התנתקות מהמשתמש הלא מאושר
+          return;
+        }
+      }
+
+      setError("");
+      navigate(`/feed`);
+    } catch (error) {
+      setError("Failed to login: " + error.message);
+    }
+  };
 
   return (
-      <div className="login-page">
-        <section className="container">
-          <div className="login-container">
-            <div className="form-container">
-              <h1 className="opacity">ברוכים השבים :)</h1>
-              <form onSubmit={(e)=>handleLogin(auth,email,password,setError,navigate)}>
-                <input
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="דואר אלקטרוני"
-                    required
-                />
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="סיסמא"
-                    required
-                />
-                <button className="opacity" type="submit">התחבר</button>
-                {error && <p style={{ color: 'red' }}>{error}</p>}
-              </form>
-              <div className="register-forget opacity">
-                <a href="/register">לא נרשמת עדיין? מהר להירשם פה!</a>
-                <a href="/reset">ממבחן למבחן קורה ששוכחים סיסמא</a>
-              </div>
+    <div className="login-page">
+      <section className="container">
+        <div className="login-container">
+          <div className="form-container">
+            <h1 className="opacity">ברוכים השבים :)</h1>
+            <form onSubmit={handleLogin}>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="דואר אלקטרוני"
+                required
+              />
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="סיסמא"
+                required
+              />
+              <button className="opacity" type="submit">
+                התחבר
+              </button>
+              {error && <p style={{ color: "red" }}>{error}</p>}
+            </form>
+            <div className="register-forget opacity">
+              <a href="/register">לא נרשמת עדיין? מהר להירשם פה!</a>
+              <a href="/reset">ממבחן למבחן קורה ששוכחים סיסמא</a>
             </div>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
